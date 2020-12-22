70f8d9a76c684a87245c8e41bd900dbbbb8c5426
Neetu Agarwal <agarwalneetu301@gmail.com>
2020-12-23 00:07:00 +0530

    product & home screen frontend done

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-reportWebVitals();
